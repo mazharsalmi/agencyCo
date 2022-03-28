93933ec0be736a8ad760a04ab202ab71f2d1aecc
mazhar <mazharsalmi@gmail.com>
2022-03-28 12:03:43 +0500

    navbar sticky updated

--- src/Componants/Navbar.js
+++ src/Componants/Navbar.js
@@ -7,38 +7,38 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
+
 import logo from "../Images/logo.png";
 import navbar from "../Componants/Navbar.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
-
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
+  
+  const [color , setColor]=useState(false) 
+  function chaingColor(){
+    if (window.scrollY >=90){
+      setColor(true)
+    }else{
+      setColor(false)
+    }
+  }
+  window.addEventListener('scroll' , chaingColor)
+  
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
 
   return (
     <AppBar position="fixed">
-      <Container maxWidth="xl" className="a">
+      <Container maxWidth="xl" className={color ? 'header header-bg': 'header'}>
         <Toolbar disableGutters>
           <Typography
             variant="h6"
